80bbe40994f524f114663184ddd45126933c3d64
Bagda@Sh <168397146+Ashwastaken78867@users.noreply.github.com>
2025-02-02 21:27:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./components/App"; // Importing the App component
 
-
-
+// Rendering the App component into the root element of the HTML
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root") // Targeting the 'root' div in index.html
+);
